679ca4b225e1e34ff93ac0e4f6d91d0966279876
Sourabh03github <sourabhwaghamode03@gmail.com>
2025-01-16 16:41:28 +0530

    New Changes 16th jan 2025 of 2 new files

--- pages/HomePage/HeroSection.jsx
+++ pages/HomePage/HeroSection.jsx
@@ -30,29 +30,3 @@ const HeroSection = () => {
 export default HeroSection;
 
 
-// import React from 'react';
-// import { Container, Typography, Button, TextField } from '@mui/material';
-
-// const HeroSection = () => {
-//   return (
-//     <Container className="hero-container">
-//       <Typography variant="h3" gutterBottom>
-//         Finding the Best Courses & Upgrade your Skill
-//       </Typography>
-//       <Typography variant="body1" paragraph>
-//         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-//       </Typography>
-//       <TextField
-//         variant="outlined"
-//         placeholder="Search What Do You Want to Learn"
-//         fullWidth
-//         style={{ marginBottom: '20px' }}
-//       />
-//       <Button variant="contained" size="large">
-//         Search
-//       </Button>
-//     </Container>
-//   );
-// };
-
-// export default HeroSection;
